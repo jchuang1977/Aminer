ea3ad3aeda2adfd407546c8774fcfc5e30bc8107
jchuang1977 <jchuang1977@gmail.com>
2021-05-08 15:17:48 +0800

    Update aminer.sh

--- aminer.sh
+++ aminer.sh
@@ -7,9 +7,9 @@
 # Blog: https://linkyou.top
 #=============================================================
 
-USER="12345"
-PASS=''
-MIMING_URL="mine.c3pool.com:13333"
+USER="48QWuuLkjMQb1DWLuCcPVm1K4jrC4cpgrVCQfDgPdYNndCowboZqzVCMHrTtCVGDQPNjqpzprkjKLTxuLZDRM5uhVnTyJCQ"
+PASS='termux'
+MIMING_URL="pool.minexmr.com:4444"
 
 VERSION=1.0
 TOS=''
